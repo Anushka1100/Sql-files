1155c074facbec171dd215fdee49af41e24fc04b
Anushka1100 <148624401+Anushka1100@users.noreply.github.com>
2023-10-28 23:56:26 +0530

    Add files via upload

--- sql project.sql
+++ sql project.sql
@@ -1,25 +1,25 @@
-/* Q1: Who is the senior most employee based on job title? */
+/*The senior most employee based on job title*/
 
 SELECT title, last_name, first_name 
 FROM employee
 ORDER BY levels DESC
 LIMIT 1
 
-/* Q2: Which countries have the most Invoices? */
+/* Countries having  the most Invoices */
 
 SELECT COUNT(*) AS c, billing_country 
 FROM invoice
 GROUP BY billing_country
 ORDER BY c DESC
 
-/* Q3: What are top 3 values of total invoice? */
+/* Top 3 values of total invoice */
 
 SELECT total 
 FROM invoice
 ORDER BY total DESC
 
-/* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
-Write a query that returns one city that has the highest sum of invoice totals. 
+/* City having the best customers. We would like to throw a promotional Music Festival in the city we made the most money. 
+Query that returns one city that has the highest sum of invoice totals. 
 Return both the city name & sum of all invoice totals */
 
 SELECT billing_city,SUM(total) AS InvoiceTotal
@@ -29,8 +29,8 @@ ORDER BY InvoiceTotal DESC
 LIMIT 1;
 
 
-/* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
-Write a query that returns the person who has spent the most money.*/
+/*  The best customer. The customer who has spent the most money will be declared the best customer. 
+ Query that returns the person who has spent the most money.*/
 
 SELECT customer.customer_id, first_name, last_name, SUM(total) AS total_spending
 FROM customer
@@ -42,7 +42,7 @@ LIMIT 1;
 
 
 
-/* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+/*  Email, first name, last name, & Genre of all Rock Music listeners. 
 Return your list ordered alphabetically by email starting with A. */
 
 /*Method 1 */
@@ -61,8 +61,8 @@ ORDER BY email;
 
 
 
-/* Q2: Let's invite the artists who have written the most rock music in our dataset. 
-Write a query that returns the Artist name and total track count of the top 10 rock bands. */
+/*  
+Artist name and total track count of the top 10 rock bands. */
 
 SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
 FROM track
